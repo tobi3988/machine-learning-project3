c80d9f4d8dd482dcaa7dde94841393d43c1b0ae6
tobi3988 <tobias.s.bachmann@gmail.com>
2014-12-13 16:08:34 +0100

    preprocessing

--- project3/learn/LetterLearner.py
+++ project3/learn/LetterLearner.py
@@ -13,61 +13,14 @@ from sklearn import svm
 
 
 class LetterLearner(EveryWordOneFeature):
+    def preprocessing(self, data):
+        pass
 
-    def fit(self, data):
-        self.data = data
-        startOfPreprocessing = time.time()
-        print "Start Preprocessing"
-        lengthOfTrainingData = self.data.shape[0]
-        print "length of trainingData = " + str(lengthOfTrainingData)
-        self.bag = BagOfWords(data)
-        self.fitting_data = self.bag.get_features_and_labels()
-        self.numberOfWords = self.fitting_data.shape[1] - 2
-        startOfFittingCities = time.time()
-        print "Finished Preprocessing in " + str((startOfFittingCities - startOfPreprocessing)) + "s"
-
-        t1 = threading.Thread(target=self.fit_cities)
-        t2 = threading.Thread(target=self.fit_countries)
-        t1.start()
-        startOfFittingCountries = time.time()
-
-        t2.start()
-        self.startOfPredictingCities = time.time()
-        t1.join()
-        t2.join()
-
-
-    def predict_cities(self):
-        print "Start predict cities"
-        start = time.time()
-        self.cityClassifier.predict(self.predict_data[:, :self.numberOfWords])
-        end = time.time()
-        print "Finished predicting cities in " + str((end - start)) + "s"
-        self.cityPrediction
-
-    def predict_countries(self):
-        start = time.time()
-        print "start predicting countries"
-        self.countryClassifier.predict(self.predict_data[:, :self.numberOfWords])
-        end = time.time()
-        print "finished predicting countries in " + str((end - start)) + "s"
-        self.countryPrediction
-
-    def predict(self, predict):
-        self.predict_data = self.bag.get_get_validation_features(predict)
-        t1 = threading.Thread(target=self.predict_cities)
-        t2 = threading.Thread(target=self.predict_countries)
-        t1.start()
-        t2.start()
-        t1.join()
-        t2.join()
-        prediction = np.vstack((self.cityPrediction, self.countryPrediction)).T
-        return prediction
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        predictor = EveryWordOneFeature()
+    def test_letter(self):
+        predictor = LetterLearner()
         crossval = CrossValidation()
         print crossval.run(predictor, 10)
 
